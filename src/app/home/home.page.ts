4d83c0c827b374607ecc94fd8bf9e6d2c9f975b8
mrzvinograd <alexvicing@mail.ru>
2025-04-19 18:33:47 +0300

    feat: add ico

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+interface Game{
+  name: string
+  rating: number
+  src: string
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -14,4 +20,9 @@ export class HomePage {
   this.mayname = this.mayname + "!"
   }
   books: string[]=["Поваренная книга анархиста","Бойцовский клуб"]
+  games:Game[]= [
+    {name:"The Elder Scrolls IV: Oblivion", rating:5, src:"/assets/images/ico.png"},
+    {name:"The Elder Scrolls V: Skyrim", rating:4.5, src:"/assets/images/kat.png"},
+    {name:"Hitman: Blood Money", rating:10, src:"/assets/images/world.png"}
+  ]
 }
